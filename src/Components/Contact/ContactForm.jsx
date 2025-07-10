ce5946616c5e66613eb560ca0d5ba5e52ded34fc
Alexander Jara <luisalexanderjara95@gmail.com>
2025-07-10 01:10:18 -0500

    Add status from email request - status 3

--- src/Components/Contact/ContactForm.jsx
+++ src/Components/Contact/ContactForm.jsx
@@ -151,7 +151,7 @@ const ContactForm = ({ logodark }) => {
                     <span className="flex item-center items-center font-extraBold xl:text-2xl text-xs">Enviar<img alt="Logo" src={logodark} className="h-6 w-auto"/></span>
                 </button>
             </div>
-            {statusMessage && <span className="text-center text-beige mt-4">{statusMessage}</span>}
+            {statusMessage && <p className="text-center text-beige mt-4">{statusMessage}</p>}
         </form>
     );
 };
